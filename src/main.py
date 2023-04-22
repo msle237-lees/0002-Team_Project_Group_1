c5cadf0404f25893cc88888747f1386afd7ce58c
wbcmac2000 <wbcmac2000@gmail.com>
2023-04-22 23:28:22 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -41,23 +41,30 @@ class Lab7GroupProject(tk.Tk):
         self.search_string_entry.bind("<FocusIn>", lambda event: self.search_string_entry.delete(0, tk.END))
         self.search_string_entry.bind("<Return>", lambda event: self.search_string_entry.focus())
 
+        #Spinbox Num Entry
+        self.nth=tk.IntVar()
+        self.spin_label = ttk.Label(self, text="Nth word: ")
+        self.spin=tk.Spinbox(self, from_=0, to=9, width=5, state='readonly')
+        self.spin_label.grid(row=3, column=0, sticky="ew")
+        self.spin.grid(row=3, column=1, sticky="ew")
+
         # and then search the text file for a string.  Create a button to accept all input and begin processing. 
         self.search_button = ttk.Button(self, text="Search", command=self.search_file)
-        self.search_button.grid(row=3, column=0, sticky="ew")
+        self.search_button.grid(row=4, column=0, sticky="ew")
         
         # Create a checkbox for case sensitivity on searching.
         self.case_sensitive = tk.BooleanVar()
         self.case_sensitive_checkbox = ttk.Checkbutton(self, text="Case sensitive", variable=self.case_sensitive)
-        self.case_sensitive_checkbox.grid(row=3, column=1, padx=15, pady=15, columnspan=1, sticky="ew")
+        self.case_sensitive_checkbox.grid(row=4, column=1, padx=15, pady=15, columnspan=1, sticky="ew")
 
         # Show the total number of times the search matched in a box.  Show the total number of words in the file.
         self.total_matches_label = ttk.Label(self, text="Total matches:")
-        self.total_matches_label.grid(row=4, column=0, padx=15, pady=15, columnspan=1, sticky="ew")
+        self.total_matches_label.grid(row=5, column=0, padx=15, pady=15, columnspan=1, sticky="ew")
         
         self.total_matches = tk.StringVar()
         self.total_matches.set("0")
         self.total_matches_label = ttk.Label(self, textvariable=self.total_matches)
-        self.total_matches_label.grid(row=4, column=3, padx=15, pady=15, columnspan=1, sticky="ew")
+        self.total_matches_label.grid(row=5, column=3, padx=15, pady=15, columnspan=1, sticky="ew")
 
         # Trap the X close button event from the window and pop up a yes/no message box "Really quit?".
         # If the user clicks "Yes", close the program.  If the user clicks "No", do nothing.
@@ -65,7 +72,7 @@ class Lab7GroupProject(tk.Tk):
 
         # Create a button to optionally save the results to a file with a filename specified in a box on a pop up window.
         self.save_button = ttk.Button(self, text="Save", command=self.save_file)
-        self.save_button.grid(row=3, column=3, sticky="ew")
+        self.save_button.grid(row=4, column=3, sticky="ew")
 
         # Trap the enter key to move the cursor to next box
         self.file_name_entry.bind('<Return>', self.focusDown)
@@ -85,9 +92,12 @@ class Lab7GroupProject(tk.Tk):
 
     def search_file(self):
         file_operations = FileOperations()
-        file_operations.open_file(self.file_name.get())
-        data = file_operations.search_file(self.search_string.get(), self.case_sensitive.get())
-        self.total_matches.set(len(data))
+        status = file_operations.open_file(self.file_name.get())
+        if status:
+            data = file_operations.search_file(self.search_string.get(), self.case_sensitive.get())
+            self.total_matches.set(len(data))
+        else:
+            self.total_matches.set("File open failure")
 
     def save_file(self):
         file_name = filedialog.asksaveasfilename()
