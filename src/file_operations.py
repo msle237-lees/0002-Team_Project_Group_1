f97f8d95de498640379a5b43a35f5a2139d98f00
wbcmac2000 <wbcmac2000@gmail.com>
2023-04-22 22:52:10 +0000

    Update file_operations.py
    attempts to open file, returning whether successful

--- src/file_operations.py
+++ src/file_operations.py
@@ -1,12 +1,23 @@
 class FileOperations:
+    OperatingFile = None
+    # Flags whether file is ready for IO
+    # flag=bool(True)
     def __init__(self):
         pass
 
     def open_file(self, file_name):
-        pass
+        # Tries to open file provided and returns whether successful
+        try:
+            OperatingFile = open(file_name, 'r')
+            return bool(True)
+        except:
+            return bool(False)
 
-    def search_file(self, search_string):
-        pass
+    def search_file(self, search_string, caseSensitive):
+        if caseSensitive:
+            pass
+        else:
+            pass
 
     def save_file(self, file_name):
         pass
